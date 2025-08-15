6b9e555f5f44ee2d21fcc4469853242b727445bf
Lezgyan <lezgyanandrew@gmail.com>
2025-08-15 17:48:16 +0400

    Создание проекта

---
+++ src/main/java/com/example/CoursTudy/CoursTudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoursTudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursTudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursTudyApplication.class, args);
+	}
+
+}
