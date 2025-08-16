2975b508ad0cd18428cf8bb25bbf32a85cf876fb
Lezgyan <lezgyanandrew@gmail.com>
2025-08-16 17:17:54 +0400

    feature: добавил подключение к базе данных postreSQL

---
+++ src/main/java/com/example/CoursTudy/config/JdbcConfig.java
@@ -0,0 +1,39 @@
+package com.example.CoursTudy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+
+    @Value("db.password")
+    private String password;
+    @Value("db.username")
+    private String username;
+
+    @Value("db.url")
+    private String url;
+
+    @Value("db.driver")
+    private String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        return DataSourceBuilder.create()
+                .driverClassName(driver)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+}
